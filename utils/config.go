ba29ddb594e3558e8e4ce16d7a05180a90b0f7ae
Andy <alvaradoandy469@gmail.com>
2026-05-24 14:04:04 -0600

    Creación de archivo Dockerfile, .dockerignore y modificación de la función LoadConfig en config.go

--- utils/config.go
+++ utils/config.go
@@ -8,7 +8,8 @@ type Config struct {
 	ServerURL         string `mapstructure:"SERVER_URL"`
 	API               string `mapstructure:"API_VERSION"`
 	TokenSymmetricKey string `mapstructure:"TOKEN_SYMMETRIC_KEY"`
-	//Usuario quemado
+
+	// Usuario quemado
 	AdminName     string `mapstructure:"ADMIN_NAME"`
 	AdminEmail    string `mapstructure:"ADMIN_EMAIL"`
 	AdminPassword string `mapstructure:"ADMIN_PASSWORD"`
@@ -19,11 +20,26 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
+
 	viper.AutomaticEnv()
+
+	viper.BindEnv("DB_DRIVER")
+	viper.BindEnv("DB_SOURCE")
+	viper.BindEnv("SERVER_URL")
+	viper.BindEnv("API_VERSION")
+	viper.BindEnv("TOKEN_SYMMETRIC_KEY")
+	viper.BindEnv("ADMIN_NAME")
+	viper.BindEnv("ADMIN_EMAIL")
+	viper.BindEnv("ADMIN_PASSWORD")
+	viper.BindEnv("ADMIN_ROLE")
+
 	err = viper.ReadInConfig()
 	if err != nil {
-		return
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return
+		}
 	}
+
 	err = viper.Unmarshal(&config)
 	return
 }
